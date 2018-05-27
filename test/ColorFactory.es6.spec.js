235374369ef26536c5647a1065da247eecf0f9a3
Helmuth Saatkamp <helmuthdu@gmail.com>
2018-05-27 20:35:37 -0300

    added flyweight pattern

---
+++ test/ColorFactory.es6.spec.js
@@ -0,0 +1,15 @@
+import { expect } from 'chai'
+import {
+  colorFactory
+} from '../src/structural/flyweight/ColorFactory.es6.js'
+
+describe('structural->flyweight: ColorFactory.es6', () => {
+  it('create a dictionary of unique colors', () => {
+    colorFactory.create('RED')
+    colorFactory.create('RED')
+    colorFactory.create('RED')
+    colorFactory.create('YELLOW')
+    colorFactory.create('YELLOW')
+    expect(Object.keys(colorFactory.colors)).to.have.lengthOf(2)
+  })
+})
