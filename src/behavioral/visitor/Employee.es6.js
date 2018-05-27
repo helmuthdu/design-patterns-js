921d70e4dfa6ee0fea218b47b082ccdd0478ee16
Helmuth Saatkamp <helmuthdu@gmail.com>
2018-05-27 20:22:33 -0300

    added visitor pattern

---
+++ src/behavioral/visitor/Employee.es6.js
@@ -0,0 +1,26 @@
+function BonusVisitor (employee) {
+  if (employee instanceof Manager) {
+    employee.bonus = employee.salary * 2
+  } else if (employee instanceof Developer) {
+    employee.bonus = employee.salary
+  }
+}
+
+class Employee {
+  bonus = 0
+  salary = 0
+
+  constructor (salary) {
+    this.salary = salary
+  }
+
+  accept (visitor) {
+    visitor(this)
+  }
+}
+
+class Manager extends Employee {}
+
+class Developer extends Employee {}
+
+export { Developer, Manager, BonusVisitor }
