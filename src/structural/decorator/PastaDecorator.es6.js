5b0f2e8f349a1a11aadf53ea0c2ec539eed09551
Helmuth Saatkamp <helmuthdu@gmail.com>
2018-05-27 20:33:13 -0300

    added decorator pattern

---
+++ src/structural/decorator/PastaDecorator.es6.js
@@ -0,0 +1,41 @@
+class Pasta {
+  constructor () {
+    this.price = 0
+  }
+
+  getPrice () {
+    return this.price
+  }
+}
+
+class Penne extends Pasta {
+  constructor () {
+    super()
+    this.price = 8
+  }
+}
+
+class PastaDecorator extends Pasta {
+  constructor (pasta) {
+    super()
+    this.pasta = pasta
+  }
+
+  getPrice () {
+    return this.pasta.getPrice()
+  }
+}
+
+class SauceDecorator extends PastaDecorator {
+  getPrice () {
+    return super.getPrice() + 5
+  }
+}
+
+class CheeseDecorator extends PastaDecorator {
+  getPrice () {
+    return super.getPrice() + 3
+  }
+}
+
+export { Penne, SauceDecorator, CheeseDecorator }
