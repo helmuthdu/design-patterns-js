3996bb6e2eb8687317b79d67927230c1b1ecf9d4
Helmuth Saatkamp <helmuthdu@gmail.com>
2018-05-27 20:30:44 -0300

    added adapter pattern

---
+++ test/JediAdapter.es6.spec.js
@@ -0,0 +1,14 @@
+import { expect } from 'chai'
+import {
+  Jedi,
+  JediAdapter,
+  Soldier
+} from '../src/structural/adapter/JediAdapter.es6.js'
+
+describe('structural->adapter: JediAdapter.es6', () => {
+  it('should create a soldier and a jedi', () => {
+    const stormtrooper = new Soldier(1)
+    const yoda = new JediAdapter(new Jedi(10))
+    expect(yoda.attack()).to.equal(stormtrooper.attack() * 1000)
+  })
+})
