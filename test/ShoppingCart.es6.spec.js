6c6c76b870ff07395e012e2903791db9c2ecd572
Helmuth Saatkamp <helmuthdu@gmail.com>
2018-05-27 20:17:09 -0300

    added strategy pattern

---
+++ test/ShoppingCart.es6.spec.js
@@ -0,0 +1,27 @@
+import { expect } from 'chai'
+import {
+  guestStrategy,
+  premiumStrategy,
+  regularStrategy,
+  ShoppingCart
+} from '../src/behavioral/strategy/ShoppingCart.es6.js'
+
+describe('behavioral->strategy: ShoppingCart.es6', () => {
+  it('should apply guest discount', () => {
+    const guestCart = new ShoppingCart(guestStrategy)
+    guestCart.setAmount(100)
+    expect(guestCart.checkout()).to.equal(100)
+  })
+
+  it('should apply regular discount', () => {
+    const regularCart = new ShoppingCart(regularStrategy)
+    regularCart.setAmount(100)
+    expect(regularCart.checkout()).to.equal(90)
+  })
+
+  it('should apply premium discount', () => {
+    const premiumCart = new ShoppingCart(premiumStrategy)
+    premiumCart.setAmount(100)
+    expect(premiumCart.checkout()).to.equal(80)
+  })
+})
