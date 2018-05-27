a50eadbac030d997ecb91d617bfb6727d240601e
Helmuth Saatkamp <helmuthdu@gmail.com>
2018-05-27 20:14:45 -0300

    added state pattern

---
+++ src/behavioral/state/Order.js
@@ -0,0 +1,33 @@
+function Order () {
+  this.state = new WaitingForPayment()
+}
+
+Order.prototype.nextState = function () {
+  this.state = this.state.next()
+}
+
+function WaitingForPayment () {
+  this.name = 'waitingForPayment'
+}
+
+WaitingForPayment.prototype.next = function () {
+  return new Shipping()
+}
+
+function Shipping () {
+  this.name = 'shipping'
+}
+
+Shipping.prototype.next = function () {
+  return new Delivered()
+}
+
+function Delivered () {
+  this.name = 'delivered'
+}
+
+Delivered.prototype.next = function () {
+  return this
+}
+
+module.exports = Order
